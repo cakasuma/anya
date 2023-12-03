56f46ebf09d6be550076af859772db9baa64aee5
cakasuma <amammustofa@gmail.com>
2023-12-04 06:35:29 +0800

    fix: include port for database

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
     host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'mysql'
 })
 
